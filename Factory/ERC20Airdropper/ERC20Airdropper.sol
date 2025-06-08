cef267f31a25623648bba01adf306738621f909f
senice222 <sasdasd920@gmail.com>
2025-06-08 18:30:53 +0300

    ERC20Airdropper added

--- Factory/ERC20Airdropper/ERC20Airdropper.sol
+++ Factory/ERC20Airdropper/ERC20Airdropper.sol
@@ -1,16 +1,21 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.29;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ERC20Airdropper {
-
+contract ERC20Airdropper is Ownable {
+    constructor() Ownable(msg.sender) { }
     IERC20 public token;
-    uint256 public amount; //100 000
+    uint256 public amount; // с учетом decimals
+    address public treasury;
 
     bool private initialized;
 
     error AlreadyInitialized();
+    error MisMatch();
+    error NotEnoughApprove();
+    error TransferFail();
 
     modifier notInitialized() {
         require(!initialized, AlreadyInitialized());
@@ -18,25 +23,29 @@ contract ERC20Airdropper {
     }
 
     function initialize(bytes memory _initData) notInitialized external returns(bool) {
-        (address _token, uint256 _amount) = abi.decode(_initData, (address, uint256));
+        (address _token, address _treasury, uint256 _amount, address _owner) = abi.decode(_initData, (address, address, uint256, address));
+
         token = IERC20(_token);
         amount = _amount;
+        treasury = _treasury;
+
+        Ownable.transferOwnership(_owner);
+
         initialized = true;
         return true;
     }
 
-    function getInitData(address _token, uint256 _amount) external pure returns(bytes memory) {
-        return abi.encode(_token, _amount);
+    function getInitData(address _token, address _treasury, uint256 _amount, address _owner) external pure returns(bytes memory) {
+        return abi.encode(_token, _treasury, _amount, _owner);
     }
 
-    function airdrop(address[] calldata receivers, uint256[] calldata amounts) external {
-        require(receivers.length == amounts.length, "arrays length mismatch");
-        require(token.allowance(msg.sender, address(this)) >= amount, "not enought approved tokens");
+    function airdrop(address[] calldata receivers, uint256[] calldata amounts) external onlyOwner {
+        require(receivers.length == amounts.length, MisMatch());
+        require(token.allowance(treasury, address(this)) >= amount, NotEnoughApprove());
 
         for (uint256 i = 0; i < receivers.length; i++) {
-            require(token.transferFrom(msg.sender, receivers[i], amounts[i]), "transfer failed");
+            require(token.transferFrom(treasury, receivers[i], amounts[i]), TransferFail());
         }
-
     }
 
 }
